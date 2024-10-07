f834f3b6080e16f5f2c12c2a372659c71ddfbb96
cgarciabrumfield <cgarciabrumfield@gmail.com>
2024-10-07 18:29:00 +0200

    combinado inigo y cris

---
+++ combat_test/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+func _process(delta: float) -> void:
+	listener()
+
+#pauses and makes the pause menu visible
+func resume():
+	
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func listener():
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
+	if Input.is_action_just_pressed("pause2") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause2") and get_tree().paused:
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
