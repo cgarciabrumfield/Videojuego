5a13df04b4655ca22dcc5e8147655b927539755a
cgarciabrumfield <cgarciabrumfield@gmail.com>
2024-10-22 16:37:38 +0200

    añadido fuentes pixelart y cambiado la escena de dialogo para que reciba un fichero de texto

--- combat_test/scripts/dialogue_interface.gd
+++ combat_test/scripts/dialogue_interface.gd
@@ -4,19 +4,23 @@ extends Control
 @onready var dialogue = $background/dialogue
 var anim_duration = 3.0
 @export var char_show_percent = 0
-var text_length = 100
-var CHARACTERS_PER_SECOND = 48
+var text_length = 320
+var CHARACTERS_PER_SECOND = 48.0
 
-func get_dialogue(dialogue_text: String):
-	if (dialogue_text != null):
-		dialogue.text = dialogue_text
-		text_length = dialogue.text.length()
+# Se tiene que llamar antes de cambiar de escena para que tenga los datos
+func get_dialogue(dialogue_file_path: String):
+	var file = FileAccess.open(dialogue_file_path, FileAccess.READ)
+	var content = file.get_as_text(true)
+	dialogue.add_text(content)
+	text_length = len(content) 
+	# Si queremos dividir el texto en vez de usar el scroll:
+	# var lines = content.split("\n") #o dividir con puntos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.speed_scale = CHARACTERS_PER_SECOND/text_length
-	animation_player.play("new_animation")
-	
+	animation_player.speed_scale = float(CHARACTERS_PER_SECOND/text_length)
+	animation_player.play("show_text")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	dialogue.set_visible_characters(char_show_percent*text_length)
