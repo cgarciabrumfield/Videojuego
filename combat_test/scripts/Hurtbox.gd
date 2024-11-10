9106cd54184485d6b84b86a85c33c09748fcab90
Iñigo <iaramburu2000@gmail.com>
2024-11-10 14:52:24 +0100

    wonwon shoot

--- combat_test/scripts/Hurtbox.gd
+++ combat_test/scripts/Hurtbox.gd
@@ -12,7 +12,11 @@ func _ready() -> void:
 # Cuando algo entra en la hurtbox, se comprueba que sea una hitbox y que no sea la nuestra
 func _on_area_entered(hitbox) -> void:
 	if hitbox == null || hitbox is not Player_Hitbox || hitbox.owner == owner:
-		return
+		if hitbox.owner is Bullet:
+			if !hitbox.owner.was_parried:
+				return
+		else:
+			return
 	# Una vez comprobado, si el dueño de la hurtbox tiene una funcion take_damage, recibe el daño
 	# que estuviera asociado a la hitbox con la que colisionó
 	if owner.has_method("take_damage"):
