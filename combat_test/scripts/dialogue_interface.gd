97df305d23c44db159d1f95fc65b3a86a85bc43b
cgarciabrumfield <cgarciabrumfield@gmail.com>
2024-11-19 16:30:31 +0100

    cambiados settings de la interfaz de dialogo del narrador para que el tamaño sea variable según la cantidad de texto

--- combat_test/scripts/dialogue_interface.gd
+++ combat_test/scripts/dialogue_interface.gd
@@ -6,6 +6,8 @@ var anim_duration = 3.0
 @export var char_show_percent = 0
 var text_length = 320
 var CHARACTERS_PER_SECOND = 48.0
+var AVERAGE_LENGTH = 65.0
+var FONT_SIZE_DEFAULT = 90
 
 # Se tiene que llamar antes de cambiar de escena para que tenga los datos
 func get_dialogue(dialogue_file_path: String):
@@ -13,6 +15,10 @@ func get_dialogue(dialogue_file_path: String):
 	var content = file.get_as_text(true)
 	dialogue.add_text(content)
 	text_length = len(content) 
+	print(text_length)
+	# tamaño de fuente variable
+	var font_size = (1.0/(text_length/AVERAGE_LENGTH)) * FONT_SIZE_DEFAULT
+	dialogue.add_theme_font_size_override("normal_font_size", font_size)
 	# Si queremos dividir el texto en vez de usar el scroll:
 	# var lines = content.split("\n") #o dividir con puntos
 
@@ -25,3 +31,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	dialogue.set_visible_characters(char_show_percent*text_length)
+	print(char_show_percent*text_length)
