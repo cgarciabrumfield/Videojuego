4f2fa84ce42a07a2b265311b666735eb0772f7cb
Iñigo <iaramburu2000@gmail.com>
2025-01-14 00:58:46 +0100

    props

---
+++ combat_test/scenes/cueva/salas/suelos/columas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="columas" tilewidth="16" tileheight="16" tilecount="80" columns="10">
+ <image source="../../../../assets/sprites/maps/Cave/Dungeon_Autotiles.png" width="160" height="128"/>
+</tileset>
